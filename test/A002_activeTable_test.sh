0aa65f583de6d0f43996e498d56721d1f4859b65
keithpinson <me@keithpinson.com>
2023-09-23 18:36:27 -0400

    🦋 test: RED test var init A002

---
+++ test/A002_activeTable_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+commit_sh="${PROJECT_ROOT}/commit.sh"
+
+testActiveTable_initialized_A002() {
+
+  test_only=true
+
+  . $commit_sh
+
+  result=$(echo $active_table | head -c10)
+
+  assertTrue "active_table is set, $result" "[ -z $result ]"
+}
+
+# Load and run shUnit2.
+. shunit2
