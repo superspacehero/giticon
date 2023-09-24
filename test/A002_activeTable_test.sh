f576d53a5ea883e3276826d54e488fadea199ff5
keithpinson <me@keithpinson.com>
2023-09-24 11:10:56 -0400

    👕 lint: Clean activeTable A002

--- test/A002_activeTable_test.sh
+++ test/A002_activeTable_test.sh
@@ -2,13 +2,17 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 commit_sh="${PROJECT_ROOT}/commit.sh"
 
+export test_only
+
 testActiveTable_initialized_A002() {
 
   test_only=true
 
-  . $commit_sh
+  # shellcheck source=${PROJECT_ROOT}/commit.sh
+  . "$commit_sh"
 
-  result=$(echo $active_table | head -c10)
+  # shellcheck disable=SC2154
+  result=$(echo "$active_table" | head -c10)
 
   assertTrue "active_table is set, $result" "[ -n $result ]"
 }
