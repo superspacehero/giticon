edfa78660f5b873cb667a0059a90739ee4d1679b
keithpinson <me@keithpinson.com>
2023-09-24 19:18:58 -0400

    ♻️ refactor: Use run_init() U001

--- test/U001_set_commit_variables_test.sh
+++ test/U001_set_commit_variables_test.sh
@@ -11,6 +11,9 @@ testNo_global_table_row_number_U001() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
+  run_init
+  run_stage_A_1_2
+
   unset table_row_number
 
   # Call with no params
@@ -26,6 +29,9 @@ testNo_global_active_table_U001() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
+  run_init
+  run_stage_A_1_2
+
   unset active_table p_wanted_row_number
 
   result=$(set_commit_variables)
@@ -40,6 +46,9 @@ testBad_param_U001() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
+  run_init
+  run_stage_A_1_2
+
   # Call with bad param
   result=$(set_commit_variables "333")
 
@@ -53,11 +62,15 @@ testFind_row_1_U001() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
+  run_init
+  run_stage_A_1_2
+
   set_commit_variables "1"
 
-  # shellcheck disable=SC2154
+   # shellcheck disable=SC2154
   assertTrue "commit variables are set, $commit_index, $commit_icon, $commit_type" \
     "[ -n $commit_index ] && [ -n $commit_type ]"
+  assertSame "commit_index is 1" "1" "$commit_index"
 }
 
 # Load and run shUnit2.
