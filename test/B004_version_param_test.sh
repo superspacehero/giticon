692c502dbcc5718515f6ed865300969f0f4224c4
keithpinson <me@keithpinson.com>
2023-09-24 18:57:16 -0400

    ✔️ fix: Undo failed git revert B004

--- test/B004_version_param_test.sh
+++ test/B004_version_param_test.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
+commit_sh="${PROJECT_ROOT}/commit.sh"
 
 testVersionParam_B004() {
-  error_num=0
+  actual="$("$commit_sh" --version)"
 
-#  actual="$("$commit_sh" --version)"
-  actual="$("${PROJECT_ROOT}"/commit.sh --version)"
-  exit_code=$?
-
-  assertSame "Expected zero exit code" "$error_num" "$exit_code"
   assertContains "$actual" ".sh v"
 }
 
