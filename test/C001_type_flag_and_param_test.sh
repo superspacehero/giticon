a3593b8da4c0c756e21c56d828bdc9358790a9e0
keithpinson <me@keithpinson.com>
2023-09-24 14:09:44 -0400

    🦋 test: RED type flag C001

--- test/C001_type_flag_and_param_test.sh
+++ test/C001_type_flag_and_param_test.sh
@@ -2,8 +2,8 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 commit_sh="${PROJECT_ROOT}/commit.sh"
 
-testBodyAndEndArgs_C001() {
-  actual="$("$commit_sh" --type hotfix -m \"Correct crashing issue#5218\" --breaking)"
+testTypeFlag_C001() {
+  actual="$("$commit_sh" --type hotfix)"
 
   assertContains "$actual" "!"
 }
