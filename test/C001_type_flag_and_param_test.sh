5a6f47209e87181e636020b76afc05166063e5cf
keithpinson <me@keithpinson.com>
2023-09-24 18:47:59 -0400

    ✔️ fix: GRN type flag C001

--- test/C001_type_flag_and_param_test.sh
+++ test/C001_type_flag_and_param_test.sh
@@ -2,10 +2,20 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 commit_sh="${PROJECT_ROOT}/commit.sh"
 
-testTypeFlag_C001() {
-  actual="$("$commit_sh" --type hotfix)"
+export test_only
 
-  assertContains "$actual" "!"
+testTypeFlag_C001() {
+
+  test_only=true
+
+  # shellcheck source=${PROJECT_ROOT}/commit.sh
+  . "$commit_sh"
+
+  run_init
+  run_stage_A_1_2 "--type" "hotfix"
+
+  assertTrue "is_flag_type is true" "$is_flag_type"
+  assertContains "flag_type contains \"hotfix\"" "$flag_type" "hotfix"
 }
 
 # Load and run shUnit2.
