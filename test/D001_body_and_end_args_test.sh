185086a8c82972f3ef704b2df2fde05aa11dd334
keithpinson <me@keithpinson.com>
2023-09-24 20:31:16 -0400

    ✔️ fix: GRN title and end args D001

--- test/D001_body_and_end_args_test.sh
+++ test/D001_body_and_end_args_test.sh
@@ -2,10 +2,30 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 commit_sh="${PROJECT_ROOT}/commit.sh"
 
-testBodyAndEndArgs_C001() {
-  actual="$("$commit_sh" --type hotfix -m \"Correct crashing issue#5218\" --breaking)"
+export test_only
 
-  assertContains "$actual" "!"
+testBodyAndEndArgs_D001() {
+  # shellcheck disable=SC2034
+  test_only=true
+
+  # shellcheck source=${PROJECT_ROOT}/commit.sh
+  . "$commit_sh"
+
+  run_init
+  run_stage_A_1_2 "do something" "--" "id:5"
+
+  # shellcheck disable=SC2154
+  assertTrue "is_argument_1 is true" "$is_argument_1"
+  # shellcheck disable=SC2154
+  assertEquals "argument_1 equals \"do something\"" "$argument_1" "do something"
+  # shellcheck disable=SC2154
+  assertTrue "is_argument_2 is true" "$is_argument_2"
+  # shellcheck disable=SC2154
+  assertEquals "argument_2 equals \"\"" "$argument_2" ""
+  # shellcheck disable=SC2154
+  assertTrue "is_argument_3 is true" "$is_argument_3"
+  # shellcheck disable=SC2154
+  assertEquals "argument_3 equals \"id:5\"" "$argument_3" "id:5"
 }
 
 # Load and run shUnit2.
