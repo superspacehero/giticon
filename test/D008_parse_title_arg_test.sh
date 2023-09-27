156408e396c94b057d2b4b19bbe5fd74b11fd756
keithpinson <me@keithpinson.com>
2023-09-27 18:07:17 -0400

    ✔️ fix: GRN parse title arg D008

--- test/D008_parse_title_arg_test.sh
+++ test/D008_parse_title_arg_test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# shellcheck disable=SC2154
+
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 commit_sh="${PROJECT_ROOT}/commit.sh"
 
@@ -11,11 +13,16 @@ testParseLoneTitle_D008() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
-  run_init
-  run_stage_A_1 "do something" "why we did it" "id:5"
+  test="do something"
 
-  # shellcheck disable=SC2154
-  assertTrue "is_argument_1 is true" false
+  run_init
+  run_stage_A_1 "$test"
+  run_stage_A_2
+
+  expected="do something"
+  actual=$arg_title
+
+  assertEquals "> $test : title equals \"$expected\"" "$expected" "$actual"
 }
 
 testParseTypeDelimiterTitle_D008() {
@@ -25,11 +32,21 @@ testParseTypeDelimiterTitle_D008() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
-  run_init
-  run_stage_A_1 "do something" "why we did it" "id:5"
+  test="fix: Correct conditional logic"
 
-  # shellcheck disable=SC2154
-  assertTrue "is_argument_1 is true" false
+  run_init
+  run_stage_A_1 "$test"
+  run_stage_A_2
+
+  expected="fix"
+  actual=$arg_type
+  assertEquals "> $test : type equals \"$expected\"" "$expected" "$actual"
+  expected=":"
+  actual=$arg_delimiter
+  assertEquals "> $test : delimiter equals \"$expected\"" "$expected" "$actual"
+  expected="Correct conditional logic"
+  actual=$arg_title
+  assertEquals "> $test : title equals \"$expected\"" "$expected" "$actual"
 }
 
 testParseScopeDelimiterTitle_D008() {
@@ -39,11 +56,21 @@ testParseScopeDelimiterTitle_D008() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
-  run_init
-  run_stage_A_1 "do something" "why we did it" "id:5"
+  test="(Android): Compensate for header"
 
-  # shellcheck disable=SC2154
-  assertTrue "is_argument_1 is true" false
+  run_init
+  run_stage_A_1 "$test"
+  run_stage_A_2
+
+  expected="Android"
+  actual=$arg_scope
+  assertEquals "> $test : scope equals \"$expected" "$expected" "$actual"
+  expected=":"
+  actual=$arg_delimiter
+  assertEquals "> $test : delimiter equals \"$expected" "$expected" "$actual"
+  expected="Compensate for header"
+  actual=$arg_title
+  assertEquals "> $test : title equals \"$expected" "$expected" "$actual"
 }
 
 testParseScopeNoDelimiterTitle_D008() {
@@ -53,11 +80,21 @@ testParseScopeNoDelimiterTitle_D008() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
-  run_init
-  run_stage_A_1 "do something" "why we did it" "id:5"
+  test="(Android) Compensate for header"
 
-  # shellcheck disable=SC2154
-  assertTrue "is_argument_1 is true" false
+  run_init
+  run_stage_A_1 "$test"
+  run_stage_A_2
+
+  expected="Android"
+  actual=$arg_scope
+  assertEquals "> $test : scope equals \"$expected" "$expected" "$actual"
+  expected=""
+  actual=$arg_delimiter
+  assertEquals "> $test : delimiter equals \"$expected" "$expected" "$actual"
+  expected="Compensate for header"
+  actual=$arg_title
+  assertEquals "> $test : title equals \"$expected" "$expected" "$actual"
 }
 
 testParseIconTypeTitle_D008() {
@@ -67,11 +104,21 @@ testParseIconTypeTitle_D008() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
-  run_init
-  run_stage_A_1 "do something" "why we did it" "id:5"
+  test="🎉 init: Add files to start search"
 
-  # shellcheck disable=SC2154
-  assertTrue "is_argument_1 is true" false
+  run_init
+  run_stage_A_1 "$test"
+  run_stage_A_2
+
+  expected="🎉"
+  actual=$arg_icon
+  assertEquals "> $test : icon equals \"$expected" "$expected" "$actual"
+  expected="init"
+  actual=$arg_type
+  assertEquals "> $test : type equals \"$expected" "$expected" "$actual"
+  expected="Add files to start search"
+  actual=$arg_title
+  assertEquals "> $test : title equals \"$expected" "$expected" "$actual"
 }
 
 testParseIconTypeScopeDelimiterTitle_D008() {
@@ -81,12 +128,40 @@ testParseIconTypeScopeDelimiterTitle_D008() {
   # shellcheck source=${PROJECT_ROOT}/commit.sh
   . "$commit_sh"
 
-  run_init
-  run_stage_A_1 "do something" "why we did it" "id:5"
+  test="⚗️ wip (sh)!Update cleanup script"
 
-  # shellcheck disable=SC2154
-  assertTrue "is_argument_1 is true" false
+  run_init
+  run_stage_A_1 "$test"
+  run_stage_A_2
+
+  expected="⚗️"
+  actual=$arg_icon
+  assertEquals "> $test : icon equals \"$expected" "$expected" "$actual"
+  expected="wip"
+  actual=$arg_type
+  assertEquals "> $test : type equals \"$expected" "$expected" "$actual"
+  expected="sh"
+  actual=$arg_scope
+  assertEquals "> $test : scope equals \"$expected" "$expected" "$actual"
+  expected="!"
+  actual=$arg_delimiter
+  assertEquals "> $test : delimiter equals \"$expected" "$expected" "$actual"
+  expected="Update cleanup script"
+  actual=$arg_title
+  assertEquals "> $test : title equals \"$expected" "$expected" "$actual"
 }
+
+#distill "🚨 hotfix:do something"
+#distill "⚗️ wip:do something"
+#distill "hotfix"
+#distill "do something"
+#distill "hotfix!Fix null ptr"
+#distill "feat (Android):Add pinch zoom"
+#distill "feat (Android) Add double tap zoom"
+#distill "(Android)"
+#distill "!"
+#distill ":"
+
 
 # Load and run shUnit2.
 . shunit2
